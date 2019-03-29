1d389921b3c2df0b6476f1ae3d81ace7e12a25e8
samashtar <sam.ashtar@flatironschool.com>
2019-03-29 18:06:04 -0400

    home route

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # create a Flask instance of the app
 app = Flask(__name__)
 
+
+# create a route
+@app.route('/')
+# points to home.html
+def index():
+    return render_template('home.html')
+
+
 # if the name matches - run the app - starting a server
+# debug mode means app updates on save
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
