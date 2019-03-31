3a2becd65ad65bfa52e96033c28b624ed1e99dd4
samashtar <sam.ashtar@flatironschool.com>
2019-03-31 17:54:40 -0400

    edited procfile

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ def send_image(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+      # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
